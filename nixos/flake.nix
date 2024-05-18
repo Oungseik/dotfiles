1ea1bbf0e51dbfaf9a39ec0c13bdb78781561126
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-05-18 20:15:51 +0630

    change to unstable branch

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
