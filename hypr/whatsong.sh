a82d97c708928837a3a48550bb0dd6391aa2eb53
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-05-22 07:39:12 +0630

    feat: add keybindings to move workspace to monitor

--- hypr/whatsong.sh
+++ hypr/whatsong.sh
@@ -1,5 +1,5 @@
 #/run/current-system/sw/bin/bash
 
-song_info=$(playerctl metadata --format '{{title}}  {{artist}}')
+song_info=$(playerctl metadata --format '{{title}}   {{artist}}')
 
 echo "$song_info" 
