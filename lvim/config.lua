b56498d51422cef16750148505eec1ac2bd8531f
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-07-13 19:16:31 +0630

    disable treesitter on large file to improve performance

--- lvim/config.lua
+++ lvim/config.lua
@@ -106,6 +106,17 @@ lvim.builtin.which_key.mappings["r"] = {
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.transparent_window = true
 
+
+lvim.builtin.treesitter.highlight.disable = function()
+  if vim.fn.strwidth(vim.fn.getline('.')) > 300
+      or vim.fn.getfsize(vim.fn.expand('%')) > 1024 * 1024 then
+    return false
+  else
+    return true
+  end
+end
+
+
 vim.o.termguicolors = true
 lvim.format_on_save = false
 
