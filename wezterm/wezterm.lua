5e8803540d6311363a680971f814efa9f6d8209b
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-09-01 09:14:00 +0630

    fix: update waybar config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,8 +7,8 @@ config.color_scheme = "Catppuccin Macchiato"
 config.enable_tab_bar = false
 config.enable_wayland = true
 
-config.font = wezterm.font("JetBrainsMono NF", { weight = "Regular", stretch = "Normal" })
-config.font_size = 12.5
+config.font = wezterm.font("JetBrainsMono NF", { weight = "Regular", stretch = "UltraExpanded" })
+config.font_size = 13
 config.front_end = "WebGpu"
 
 config.window_background_opacity = 0.95
